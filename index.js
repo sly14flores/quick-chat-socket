b8ecc7ed82878b5cbab5cc916d2e09f276b2cce6
Sly Flores <sly@christian.com.ph>
2021-10-12 17:57:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-const httpServer = createServer();
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 80;
+// const INDEX = '/index.html';
+
+const server = express()
+  // .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
 
 //
 io.on("connection", (socket) => {
@@ -27,7 +31,3 @@ io.on("connection", (socket) => {
   })
 });
 //
-
-httpServer.listen(80, () => {
-  console.log("Socket server started")
-});
