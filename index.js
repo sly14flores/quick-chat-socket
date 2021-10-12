b29fef744a9aed2f3bfd71f26c9fbaa1a30bfb95
Sly Flores <sly@christian.com.ph>
2021-10-12 11:01:17 +0800

    ...

---
+++ index.js
@@ -0,0 +1,31 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log(`${socket.id} is connected`)
+  socket.on('message', (payload) => {
+    // console.log(payload)
+    socket.broadcast.emit('getConvo', payload)
+  })
+  socket.on('login', (payload) => {
+    console.log(`${payload.name} has login`)
+    socket.broadcast.emit('userLogin', payload)
+  })
+  socket.on('logout', (payload) => {
+    socket.broadcast.emit('userLogin', payload)
+  })
+  socket.on('chat', (payload) => {
+    socket.broadcast.emit('chatFocus', payload)
+  })
+});
+
+httpServer.listen(80, () => {
+  console.log("Socket is live")
+});
